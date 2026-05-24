79c49c88c315ca80462fdee6f183ed2597258f7c
José Manuel Sánchez Muñoz <jose.sanchezm937@gmail.com>
2026-05-24 14:23:24 -0600

    primeros archivos
    primeros archivos

---
+++ sample_project/main/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+
+#define led_pin 2
+
+void app_main(void)
+{
+    gpio_reset_pin(led_pin);
+    gpio_set_direction(led_pin, GPIO_MODE_OUTPUT);
+
+    while(1){
+        gpio_set_level(led_pin, true);
+        vTaskDelay(500/portTICK_PERIOD_MS);
+
+        gpio_set_level(led_pin, false);
+        vTaskDelay(500/portTICK_PERIOD_MS);
+    }
+
+}
