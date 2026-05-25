866aedbfa8f754f0fca0cf91fd5ea1612c078770
José Manuel Sánchez Muñoz <jose.sanchezm937@gmail.com>
2026-05-24 20:25:29 -0600

    starting the spi comunication example

--- Frimware_tests/SPI_Test/main/main.c
+++ Frimware_tests/SPI_Test/main/main.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
+#include "esp_log.h"
+#include "freertos/task.h"
+#include "driver/spi_master.h"
 
-#define led_pin 2
+#define MISO 13
+#define MOSI 11
+#define SCLK 12
+#define CS 10
+
+static esp_err_t init_spi(void){
+
+    spi_bus_config_t spi_bus_config = {
+        
+        .miso_io_num = MISO,
+        .mosi_io_num = MOSI,
+        .sclk_io_num = SCLK,
+        
+    }
+
+
+    spi_bus_initialize(SPI2_HOST, )
+    return ESP_OK
+}
 
 void app_main(void)
 {
-    gpio_reset_pin(led_pin);
-    gpio_set_direction(led_pin, GPIO_MODE_OUTPUT);
 
-    while(1){
-        gpio_set_level(led_pin, true);
-        vTaskDelay(1000/portTICK_PERIOD_MS);
 
-        gpio_set_level(led_pin, false);
-        vTaskDelay(1000/portTICK_PERIOD_MS);
-    }
 
 }
