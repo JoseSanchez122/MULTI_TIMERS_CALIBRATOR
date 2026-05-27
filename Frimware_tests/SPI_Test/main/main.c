36ecffe80c135eb1426c2c351818cfe377b6fce1
José Manuel Sánchez Muñoz <jose.sanchezm937@gmail.com>
2026-05-27 12:50:57 -0600

    Update main.c

--- Frimware_tests/SPI_Test/main/main.c
+++ Frimware_tests/SPI_Test/main/main.c
@@ -20,7 +20,7 @@ static esp_err_t init_spi(void){
         .sclk_io_num = SCLK,
         .quadwp_io_num = NOT_USED,   
         .quadhd_io_num = NOT_USED,   
-        .data4_io_num = NOT_USED,    
+        .data4_io_num = NOT_USED,      
         .data5_io_num = NOT_USED,    
         .data6_io_num = NOT_USED,    
         .data7_io_num = NOT_USED,    
@@ -29,11 +29,21 @@ static esp_err_t init_spi(void){
     };
 
     spi_device_interface_config_t spi_device_interface_config = {
+        .command_bits = 8,
+        .address_bits = 0,
+        .dummy_bits = 0,
+        .mode = 0,
+        .duty_cycle_pos = 128, 
+        .clock_speed_hz = SPI_MASTER_FREQ_8M,
+        .input_delay_ns = 0,
+        .spics_io_num = CS,
+        
+
         
     };
 
     spi_bus_initialize(SPI2_HOST, &spi_bus_config, SPI_DMA_DISABLED);
-    //spi_bus_add_device(SPI2_HOST, )
+    spi_bus_add_device(SPI2_HOST, );
 
     return ESP_OK;
 }
